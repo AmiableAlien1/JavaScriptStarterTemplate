6a0db7f81ee0ae40d16b1c3e26ece6fd8731bcb9
Peter 12 <phamp1@s.dcsdk12.org>
2022-08-22 16:06:27 -0600

    Added variables and started the guessing game

---
+++ scripts/guessing-game.js
@@ -0,0 +1,20 @@
+//@ts-check
+
+const button = document.getElementById("guessing-game");
+
+console.log(button);
+
+button?.addEventListener("click", function(){
+   // console.log("Clicked!");
+let answer = 7;
+
+   let guess = Number(
+    prompt("Pick a number between 1 and 10")
+ );
+
+if (answer == guess) {
+    alert("You are correct!")
+}
+
+    console.log(guess);
+});
---
+++ scripts/variables.js
@@ -0,0 +1,26 @@
+// boolean are only true or false
+
+let isThisClassAwesome = true;
+
+// console.log(myName) ;
+
+let car = {
+    year: 2006,
+    make: "Toyota",
+    model: "Highlander",
+    isClean: false,
+}
+
+car.year = 2001;
+car.color = "black"
+console.log(car);
+
+//declare an array of fruits
+let fruits = [
+    "apple",
+    "pear",
+    "grapes",
+    "banana",
+];
+
+console.log(fruits[2]);
